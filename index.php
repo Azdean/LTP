63f80c24c944ad996397fb68c3b82f21bad2e4f8
Azdean <azdean.samihgmail.comls -al ~/.sshssh-keygen -t rsa -b 4096 -C your_email@example.comgit config --global user.email azdean.samihgmail.com>
2015-10-12 20:13:59 +0100

    Setup of core files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- -
+- Lung Treatment Plan v0.1 
+- Author: Azdean Samih
+- Mission: Create a simple website that can be used by nurses and doctors to produce a simple
+- text free treatment plan to assist patients with poor sight or literacy ability. 
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LTP: Lung Treatment Plan</title>
+	<link rel="stylesheet" type="text/css" href="./src/style/styles.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+		<h1>LTP</h1>
+	</header>
+</body>
+</html>
