2a1216ad8793c93084f3278f7d9ccbef00d563b4
Azdean <azdean.samihgmail.comls -al ~/.sshssh-keygen -t rsa -b 4096 -C your_email@example.comgit config --global user.email azdean.samihgmail.com>
2015-10-13 21:00:06 +0100

    Basic themeing and misc structure

--- index.php
+++ index.php
@@ -10,11 +10,32 @@
 <head>
 	<title>LTP: Lung Treatment Plan</title>
 	<link rel="stylesheet" type="text/css" href="./src/style/styles.css">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-</head>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'></head>
 <body>
 	<header>
 		<h1>LTP</h1>
 	</header>
+
+	<nav>
+	<li>Column One</li>
+	<li>Column Two</li>
+	<li>Column Three</li>
+	<li>Column Four</li>
+	<li>Column Five</li>
+	</nav>
+
+	<section class="main">
+		<section class="table">
+
+			<div class="table-head">
+			</div>
+
+			<div class="table-column">
+				<div class="img-container"></div>
+			</div>
+
+		</section>
+	</section>
+
 </body>
 </html>
